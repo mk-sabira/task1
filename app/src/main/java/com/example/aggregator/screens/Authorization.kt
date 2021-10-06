1a4a42a26d58055feb5550910f681f6d633bdeba
Sabira <mnsabira@gmail.com>
2021-10-06 14:43:47 +0400

    worked on Registration

--- app/src/main/java/com/example/aggregator/screens/Authorization.kt
+++ app/src/main/java/com/example/aggregator/screens/Authorization.kt
@@ -2,6 +2,7 @@ package com.example.aggregator.screens
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,6 +30,9 @@ class Authorization : Fragment(), AdapterView.OnItemSelectedListener {
         spinner()
         openScreenForgotPassword()
         openScreenRegistration()
+
+    }
+
     }
 
     private fun openScreenRegistration() {
--- app/src/main/java/com/example/aggregator/screens/Registration.kt
+++ app/src/main/java/com/example/aggregator/screens/Registration.kt
@@ -1,22 +1,38 @@
 package com.example.aggregator.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.aggregator.R
+import com.example.aggregator.databinding.FragmentAuthorizationBinding
+import com.example.aggregator.databinding.FragmentRegistrationBinding
 
 
 class Registration : Fragment() {
 
-
+    lateinit var binding: FragmentRegistrationBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        binding = FragmentRegistrationBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        getData()
+    }
+
+    private fun getData() {
+        binding.btnSave.setOnClickListener {
+            val userName = binding.edUserName.text
+            val userSurname = binding.edUserSurname.text
+            val userEmail = binding.edEmail.text
+            Log.d("MyLog", "$userName + $userSurname + $userEmail")
+        }
     }
 
     companion object{
